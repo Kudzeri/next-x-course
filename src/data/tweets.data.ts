cc97895f3b8248bd1053de61888e3dd92b4ff87c
Kudzeri <kudzeri@gmail.com>
2025-06-27 08:53:19 +0500

    add tweets data and tweet interface

---
+++ src/data/tweets.data.ts
@@ -0,0 +1,28 @@
+import type { ITweet } from "@/shared/types/tweet.interface";
+
+export const TWEETS: ITweet[] = [
+  {
+    author: "tonystark2",
+    text: "I am webdeveloper from Kazakhstan. Hello, world!",
+  },
+  {
+    author: "brucewayne22",
+    text: "I am a web developer from Gotham City. Hello, world!",
+  },
+  {
+    author: "clarkkent34",
+    text: "I love react and nextjs. Do you?",
+  },
+  {
+    author: "peterparker12",
+    text: "For security reasons, I can't tell you who I am.",
+  },
+  {
+    author: "steverogers",
+    text: "Sometimes I feel like I'm the only one who doesn't know how to use React.",
+  },
+  {
+    author: "natasharomanoff",
+    text: "I am a web developer from Russia. Hello, world!",
+  },
+];
