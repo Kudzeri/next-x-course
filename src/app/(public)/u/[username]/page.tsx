c1766eeef571f2039b3a8a3af71253f3a2721636
Kudzeri <kudzeri@gmail.com>
2025-06-27 10:49:22 +0500

    update src/app structure to include public routes

---
+++ src/app/(public)/u/[username]/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+    title: 'Profile',
+    description: 'User profile page',
+}
+
+export default function ProfilePage() {
+    return <div>Profile</div>
+}
