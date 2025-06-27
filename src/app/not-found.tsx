5be3d8735698ba7d0fa1a5cfa225e291f63f1a74
Kudzeri <kudzeri@gmail.com>
2025-06-27 19:58:46 +0500

    update not found page to include a header component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,21 @@
+import { Header } from "@/components/Header";
 import { PAGES } from "@/config/pages.config";
 import Link from "next/link";
 
 export default function notFound() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center text-center px-4">
-      <h1 className="text-7xl font-bold mb-4">404</h1>
-      <p className="text-neutral-600 mb-6">
-        This page doesn't exist or has been removed.
-      </p>
-      <Link href={PAGES.HOME.href} className="text-blue-500 hover:underline">
-        ← Go back to Home
-      </Link>
-    </div>
+    <>
+      <Header />
+
+      <div className="min-h-screen flex flex-col items-center justify-center text-center px-4">
+        <h1 className="text-7xl font-bold mb-4">404</h1>
+        <p className="text-neutral-600 mb-6">
+          This page doesn't exist or has been removed.
+        </p>
+        <Link href={PAGES.HOME.href} className="text-blue-500 hover:underline">
+          ← Go back to Home
+        </Link>
+      </div>
+    </>
   );
 }
