16534f6b5a7f64aea34314e1134b14e6133c08f7
Kudzeri <kudzeri@gmail.com>
2025-06-28 20:03:11 +0500

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://dummyjson.com",
+  },
   images: {
     domains: ["cdn.dummyjson.com"],
   },
