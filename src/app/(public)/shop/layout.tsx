cd7ef9a222555405e8a5703ff1f3c5ae705c4e21
Kudzeri <kudzeri@gmail.com>
2025-06-28 20:07:27 +0500

    add dynamic import for ShopMenu component

--- src/app/(public)/shop/layout.tsx
+++ src/app/(public)/shop/layout.tsx
@@ -1,11 +1,18 @@
+"use client";
+
+//ctrl+shift+p to open command palette
+import dynamic from "next/dynamic";
 import type { PropsWithChildren } from "react";
-import { ShopMenu } from "./ShopMenu";
+const DynamicShopMenu = dynamic(
+  () => import("./ShopMenu").then((mod) => mod.ShopMenu),
+  { ssr: false, loading: () => <div>Loading...</div> }
+);
 
 export default function layout({ children }: PropsWithChildren<unknown>) {
   return (
     <div className="w-full">
       <h1 className="text-3xl font-bold mb-6">Shop</h1>
-      <ShopMenu />
+      <DynamicShopMenu />
       {children}
     </div>
   );
