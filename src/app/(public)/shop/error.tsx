2ce1e907d63d9a2d73418a56720a4366fb96a730
Kudzeri <kudzeri@gmail.com>
2025-06-28 19:44:36 +0500

    ...

--- src/app/(public)/shop/error.tsx
+++ src/app/(public)/shop/error.tsx
@@ -1,4 +1,5 @@
 "use client";
+//для SSR
 
 export default function Error({ error }: { error: Error }) {
   return (
--- src/app/(public)/shop/loading.tsx
+++ src/app/(public)/shop/loading.tsx
@@ -1,4 +1,5 @@
 export default function Loading() {
+  //для SSR
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-white-500"></div>
